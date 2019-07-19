f65f7e3e5bc7fe86470b24fd4340c07292079be8
tbmao2 <158972928@qq.com>
2019-07-19 11:49:10 +0800

    详情页添加loading

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: '电影',
       theme: ThemeData(
         primarySwatch: Colors.blue,
